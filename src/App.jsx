8e273df1d67167ba5e8c59f2da72c02921e35f8c
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-26 15:47:40 +0700

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import About from './components/About';
-import Courses from './components/Courses';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import FloatingWhatsApp from './components/FloatingWhatsApp';
-import ScrollToTop from './components/ScrollToTop';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <About />
-      <Courses />
-      <Testimonials />
-      <Contact />
-      <Footer />
-      <ScrollToTop />
-      <FloatingWhatsApp />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
