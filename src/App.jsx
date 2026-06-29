8a1dd7f72ba3db5b64b034365ec4b5979fd47a7c
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-29 23:39:48 +0700

    Add route for InHouseTrainingJakarta page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import InHouseTrainingJakarta from "./pages/InHouseTrainingJakarta";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      
+      <Route
+        path="/inhouse-training-jakarta"
+        element={<InHouseTrainingJakarta />}
+      />
     </Routes>
   );
 }
