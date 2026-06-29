677853ec6a044e71872a3168c650ed68a01a6652
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-29 23:49:22 +0700

    Remove InHouseTrainingJakarta route from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import InHouseTrainingJakarta from "./pages/InHouseTrainingJakarta";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/inhouse-training-jakarta"
-        element={<InHouseTrainingJakarta />}
-      />
+      
     </Routes>
   );
 }
