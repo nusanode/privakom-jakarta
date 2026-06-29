a666f4cb1222a01334e2ac4fc3b1a6bf5f7a232c
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-29 23:26:39 +0700

    Remove InHouseTrainingJakarta route from App
    Removed InHouseTrainingJakarta route from App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// PERBAIKAN: Mengubah Inhouse menjadi InHouse (menggunakan T besar)
-import InHouseTrainingJakarta from "./pages/InHouseTrainingJakarta";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/inhouse-training-jakarta"
-        element={<InHouseTrainingJakarta />}
-      />
+      
     </Routes>
   );
 }
