ce960601662c038d35a5127e5b44f94f6d96f348
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-29 09:21:00 +0700

    Add route for KursusKomputerTangerang page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import KursusKomputerTangerang from "./pages/KursusKomputerTangerang";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route
+  path="/kursus-komputer-tangerang"
+  element={<KursusKomputerTangerang />}
+/>
     </Routes>
   );
 }
