d47e174008dccee31d0f10cee30c38b8295127ce
csprivakom-ctrl <cs.privakom@gmail.com>
2026-08-27 16:03:29 +0700

    Refactor import statements and reformat Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,24 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import About from "../components/About";
-import Courses from "../components/Courses";
-import Testimonials from "../components/Testimonials";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import FloatingWhatsApp from "../components/FloatingWhatsApp";
-import ScrollToTop from "../components/ScrollToTop";
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import About from '../components/About';
+import Courses from '../components/Courses';
+import Testimonials from '../components/Testimonials';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import FloatingWhatsApp from '../components/FloatingWhatsApp';
+import ScrollToTop from '../components/ScrollToTop';
 
 function Home() {
   return (
     <>
       <Navbar />
-
-      <main>
-        <Hero />
-        <Features />
-        <About />
-        <Courses />
-        <Testimonials />
-        <Contact />
-      </main>
-
+      <Hero />
+      <Features />
+      <About />
+      <Courses />
+      <Testimonials />
+      <Contact />
       <Footer />
       <ScrollToTop />
       <FloatingWhatsApp />
